65e522ac9f20ebb73ae5b26759a8c4c3581511ac
AndresNei20 <andresfelipeneira2004@gmail.com>
2024-09-06 12:08:00 -0500

    css and details of SelectionCard

--- src/utils/icons.jsx
+++ src/utils/icons.jsx
@@ -52,8 +52,8 @@ export const IconComponents = {
   shop: ({ fill }) => {
     return(
       <svg
-      width="17"
-      height="17"
+      width="24"
+      height="24"
       viewBox="0 0 17 17"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
