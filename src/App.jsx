61363a095d5a6b1aeec4d42c46f6c781c1bb9a08
SantiagoV0106 <velascosantiago3@gmail.com>
2024-09-24 19:16:43 -0500

    CardState color adjust

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { CardState } from "./components/Controls/CardState"
+
 function App() {
   return (
-  <h1>Hello UXLab / Storybook</h1>
+    <>
+      <h1>Hello UXLab / Storybook</h1>
+      <CardState state='notCompleted' text='Estado' iconName='checkIcon' showIcon={false} />
+    </>
   )
 }
 
--- src/components/Controls/CardState.jsx
+++ src/components/Controls/CardState.jsx
@@ -4,15 +4,15 @@ import "./CardState.css";
 
 export const CardState = ({ text, iconName, showIcon = true, state}) => {
 
-    const stateClasses = {
-        completed: "bg-system-successColor", 
-        inProgress: "bg-system-warningColor", 
-        notCompleted: "bg-system-errorColor", 
-      };
+    // const stateClasses = {
+    //     completed: "bg-system-successColor", 
+    //     inProgress: "bg-system-warningColor", 
+    //     notCompleted: "bg-system-errorColor", 
+    //   };
 
   return (
     <div className="card-container">
-      <div className={`icon-container ${stateClasses[state]}`}>
+      <div className={`icon-container ${state === 'completed' ? 'bg-system-successColor' : state === 'inProgress' ? 'bg-system-wariningColor' : 'bg-system-errorColor' } `}>
       {showIcon && <Icon name={iconName} fill="currentColor" />}
       </div>
       <p className="inner-text">{text}</p>
